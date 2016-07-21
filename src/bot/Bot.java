f487d2a8a2a3b85754058fbb84e669e685b1be01
Semyon Bessonov <semeon-bessonov@yandex.ru>
2016-07-21 15:57:14 +0300

    Bot editing, mow frst move always pawn, next 4 - random;

--- src/bot/Bot.java
+++ src/bot/Bot.java
@@ -10,27 +10,49 @@ import java.util.Random;
  * Created by Semyon Bessonov on 20.07.2016.
  */
 public class Bot extends Player {
-
+    int moveCounter = 0;
     public Bot(String name, Color color) {
+
         super(name, color);
     }
+
+
 
     public Move nextMove(Table table){
 
         List<Figure> figureList = table.getFiguresByColor(color);
+        Move nextMove = null;
+
+        if (moveCounter < 5){ //First 4 stating moves will be random;
+            Random randomGenerator = new Random();
+
+            if (moveCounter == 0){
+                int pawnPosition = randomGenerator.nextInt(8);
+                if (this.color == color.WHITE)
+                {
+                    return new Move(new Coordinate(1, pawnPosition), new Coordinate(4, pawnPosition));
+                }else{
+                    return new Move(new Coordinate(6, pawnPosition), new Coordinate(4, pawnPosition));
+                }
+            }
+
+
+            boolean found = false;
+            while (!found){
+                List<Move> movies = figureList.get(randomGenerator.nextInt(figureList.size())).getPossibleMoves();
+                if (movies.size() > 0){
+                    found = true;
+                    return movies.get(randomGenerator.nextInt(movies.size()));
+                }
+            }
+        }
+
         Evaluation evaluation = new Evaluation();
-        Random randomGenerator = new Random();
-
-        int maxRank  = -90000;
-
-        Move nextMove  =  figureList.get(0).getPossibleMoves().get(0);
-
-        //Not best, but prevent no any move situation
+        int maxRank  = -90000; //Not best, but prevent no any move situation
 
         for (Figure f: figureList){
             Coordinate position = f.getCoor();
             List<Move> moves = f.getPossibleMoves();
-
 
             for (Move move: moves){
                 Table newTable = table.clone();
@@ -42,6 +64,8 @@ public class Bot extends Player {
                 }
             }
         }
+
+        this.moveCounter++;
         return nextMove;
     }
 
